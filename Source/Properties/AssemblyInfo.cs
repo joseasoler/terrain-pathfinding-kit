30dbb67fab8f97dec9b82fa7e72a78d2b6ae1858
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-04-24 07:50:43 +0200

    C# project files.

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TerrainPathfindingKit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TerrainPathfindingKit")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25B91723-8F2F-4CF1-97B4-7788C74B1D9D")]
+
+[assembly: AssemblyVersion ("0.1.0")]
